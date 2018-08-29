bc6274201c614dbb7f01d346234865a081e590f2
M1047737 <M1047737@B2ML28044>
2018-08-29 18:07:17 +0530

    Updated ViewProductDetails Code

--- ECommerceSite/EcommerceJS/viewproduct.js
+++ ECommerceSite/EcommerceJS/viewproduct.js
@@ -1,4 +1,14 @@
-﻿$(document).ready(function () {
+﻿
+var imgageId = "#productImage";
+var prdNameId = "#prdName";
+var priceId = "#divPrice";
+var descId = "#desc";
+var modelNameId = "#tdModelName";
+var simTypeId = "#simTypeName";
+var ramId = "#tdRam";
+var btryId = "#btryName";
+
+$(document).ready(function () {
     var ProductData = {
         ProductId: productId
     }
@@ -8,6 +18,14 @@
         data: ProductData,
         dataType: 'json',
         success: function (data) {
+            $(imgageId).attr("src", ImgPath + data[0].ImageName);
+            $(prdNameId).html(data[0].ProductName);
+            $(priceId).html(data[0].CurrencyName + " " + data[0].Price);
+            $(descId).html(data[0].Description);
+            $(modelNameId).html(data[0].ModelName);
+            simTypeId == SimType.Single ? $(simTypeId).html("Single") : $(simTypeId).html("Double");
+            $(ramId).html(data[0].RAM + "GB");
+            $(btryId).html(data[0].BatteryCapacity + "mAh");
             //alert('hi')
             //GetProducts(AdminProducts);
         }, error: function (err) {
